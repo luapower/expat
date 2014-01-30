274e94a97165687ff8b6c44ac54181af78ff5907
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-01-30 16:35:28 +0000

    linux64 build

---
+++ csrc/expat/build-linux32.sh
@@ -0,0 +1,1 @@
+gcc -O2 -s -static-libgcc *.c -shared -o ../../bin/linux32/libexpat.so -I. -DHAVE_EXPAT_CONFIG_H
--- csrc/expat/build-mingw32.sh
+++ csrc/expat/build-mingw32.sh
@@ -1,1 +1,1 @@
-
+gcc -O2 -s -static-libgcc *.c -shared -o ../../bin/mingw32/expat.dll -I.
