910811bb914bdcd0584ea9deb12528303a68cfe2
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-19 23:31:04 +0200

    osx32 build

---
+++ csrc/expat/build-osx32.sh
@@ -0,0 +1,1 @@
+gcc -arch i386 -O2 src/*.c -shared -o ../../bin/osx32/libexpat.dylib -Isrc -DHAVE_EXPAT_CONFIG_H
--- csrc/expat/build-osx64.sh
+++ csrc/expat/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -O2 src/*.c -shared -o ../../bin/osx64/libexpat.dylib -Isrc -DHAVE_EXPAT_CONFIG_H
+gcc -arch x86_64 -O2 src/*.c -shared -o ../../bin/osx64/libexpat.dylib -Isrc -DHAVE_EXPAT_CONFIG_H
