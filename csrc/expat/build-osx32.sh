eb2c82231c3160cab13d8af5a64ee2676ba5e39d
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:06:48 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/expat/build-osx32.sh
+++ csrc/expat/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 src/*.c -shared -o ../../bin/osx32/libexpat.dylib -Isrc -DHAVE_EXPAT_CONFIG_H
+gcc -arch i386 -O2 src/*.c -shared -install_name @loader_path/libexpat.dylib -o ../../bin/osx32/libexpat.dylib -Isrc -DHAVE_EXPAT_CONFIG_H
