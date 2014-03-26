5ac3b308dd03853851452bdc0caa9f98d36d43de
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:06:48 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/expat/build-osx64.sh
+++ csrc/expat/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 src/*.c -shared -o ../../bin/osx64/libexpat.dylib -Isrc -DHAVE_EXPAT_CONFIG_H
+gcc -arch x86_64 -O2 src/*.c -shared -install_name @loader_path/libexpat.dylib -o ../../bin/osx64/libexpat.dylib -Isrc -DHAVE_EXPAT_CONFIG_H
