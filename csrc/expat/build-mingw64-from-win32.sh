2ed2931834bc21a2883535f7864de1977190fe99
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-11-09 21:56:49 +0200

    static libs

--- csrc/expat/build-mingw64-from-win32.sh
+++
@@ -1,1 +0,0 @@
-i686-w64-mingw32-gcc -O2 -s -static-libgcc src/*.c -shared -o ../../bin/mingw64/expat.dll -Isrc -DHAVE_EXPAT_CONFIG_H
--- csrc/expat/build-mingw64.sh
+++ csrc/expat/build-mingw64.sh
@@ -1,1 +1,1 @@
-gcc -O2 -s -static-libgcc src/*.c -shared -o ../../bin/mingw64/expat.dll -Isrc -DHAVE_EXPAT_CONFIG_H
+P=mingw64 L="-s -static-libgcc" D=expat.dll A=expat.a ./build.sh
